41dcc0a326a614c65d7ea3f6c9ff06bf50682e7e
Kristjan Jansen <kristjan.jansen@gmail.com>
2023-08-14 17:30:08 +0300

    color fixes v2

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,5 @@
 <template>
-  <Body class="bg-orange-300" />
-  <div class="w-full border border-black/30 border-2 p-4">
+  <div class="w-full border-black/30 border-2 p-4">
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,15 @@ const domain = "https://heldinhuman.elektron.art";
     <h1 class="text-4xl font-bold uppercase">Held In Human backend</h1>
     <div class="grid md:grid-cols-2 gap-6">
       <NuxtLink to="/chat">
-        <Card class="hover:bg-orange-200/80">
+        <Card>
           <Stack>
             <h1 class="font-bold text-xl break-all">{{ domain }}/chat</h1>
             <p>Embeddable chat widget</p>
           </Stack>
         </Card>
       </NuxtLink>
-      <NuxtLink to="/chat">
-        <Card class="bg-orange-200 hover:bg-orange-200/80">
+      <NuxtLink to="/q">
+        <Card>
           <Stack>
             <h1 class="font-bold text-xl break-all">{{ domain }}/q</h1>
             <p>Embeddable widget for showing last chat message</p>
@@ -23,7 +23,7 @@ const domain = "https://heldinhuman.elektron.art";
         </Card>
       </NuxtLink>
       <NuxtLink to="/history">
-        <Card class="bg-orange-200 hover:bg-orange-200/80">
+        <Card>
           <Stack>
             <h1 class="font-bold text-xl">/history</h1>
             <p>Embeddable widget for showing last chat message</p>
